55e417f59d5aa60496abc2d43a05c5cf435b9969
DenysPetrenk0 <p.gesser2015@gmail.com>
2021-06-30 23:47:27 +0300

    homework11 part1 done

---
+++ index.js
@@ -0,0 +1,28 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+const body = document.querySelector('body');
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+let intervalId;
+
+body.addEventListener('click', onclick);
+
+function onclick(e) {
+  const nameBtn = e.target.dataset.action;
+  if (nameBtn === 'stop') {
+    clearInterval(intervalId);
+    return;
+  }
+  intervalId = setInterval(() => {
+    body.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, colors.length)];
+  }, 1000);
+}
